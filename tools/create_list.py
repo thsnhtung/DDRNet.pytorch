f4b5109117ded20b6b8a6c4ec7e5a14f2496237d
thsnhtung <19522491@gm.uit.edu.vn>
2021-08-21 09:47:40 +0700

    Modify lst file

--- tools/create_list.py
+++ tools/create_list.py
@@ -1,19 +1,19 @@
 import os 
 
 
-root = r'C:\Users\Asus\Desktop\AI\SegmentData\FullData'
+root = r'C:\Users\Asus\Desktop\AI\SegmentData\Simulation'
 
 list_dir = r'C:\Users\Asus\Desktop\DDRNet.pytorch'
 
-train_dir = os.path.join(root, 'train')
+train_dir = os.path.join(root, 'test')
 
 img_dir = os.path.join(train_dir, 'image')
 label_dir = os.path.join(train_dir, 'label')
 
-out_file = open(os.path.join(list_dir, "train.txt"), "w") 
+out_file = open(os.path.join(list_dir, "test.txt"), "w") 
 
 for file in os.listdir(img_dir):
-    data_line = "image/" + file + "\t" + "label/" + file + "\n"
+    data_line = "test/image/" + file + "\t" + "test/label/" + file + "\n"
     out_file.write(data_line) 
 
 out_file.close()
